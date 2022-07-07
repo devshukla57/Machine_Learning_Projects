b420fd547d5e15d05e0a58687836a6816d174106
Devendra Shukla <devshukla2012@gmail.com>
2022-07-07 12:34:26 +0530

    3rd july classes done

--- demo.py
+++ demo.py
@@ -10,8 +10,8 @@ def main():
         pipeline.run_pipeline()
         # # data_validation_config = Configuartion().get_data_transformation_config()
         # # print(data_validation_config)
-        # schema_file_path=r"D:\Project\machine_learning_project\config\schema.yaml"
-        # file_path=r"D:\Project\machine_learning_project\housing\artifact\data_ingestion\2022-06-27-19-13-17\ingested_data\train\housing.csv"
+        # schema_file_path=r"C:\Users\devsh\OneDrive\Desktop\Project\machine_learning_project\config\schema.yaml"
+        # file_path=r"C:\Users\devsh\OneDrive\Desktop\Project\machine_learning_project\housing\artifact\data_ingestion\2022-07-07-09-08-51\ingested_data\train\housing.csv"
 
         # df= DataTransformation.load_data(file_path=file_path,schema_file_path=schema_file_path)
         # print(df.columns)
